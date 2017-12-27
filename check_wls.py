1ecc02c05b272bbd6f7dfd3e6feea9db55a4675f
Harald Dahl <haralddahl@Haralds-MacBook-Pro-2.local>
2017-12-27 18:19:03 +0100

    Add password and username as arguments instead of variables in the code

--- check_wls.py
+++ check_wls.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python
 import urllib2, json, base64, sys
 
-#Weblogic user with monitoring rights
-username = "nagios"
-password = ""
-
-
 (EXIT_OK, EXIT_WARNING, EXIT_CRITICAL, EXIT_UNKNOWN) = (0,1,2,3)
 
 def main():
     try:
         #Check if correct parameters are entered
-        if len(sys.argv) is 4:
-            if sys.argv[1]=='--help':
-                print("The syntax is: [server_address] [port] [check_type] [warning_threshold] [critical_threshold]")
-                print("Check types: totalthread, threadhealth, stuckthreads and serverhealth")
-                print("Do not specify thresholds for serverhealth and threadhealth")
+        if len(sys.argv) is 2:
+            if sys.argv[1] == "--help":
+                print("The syntax is: [server_address] [port] [username] [password] [check_type] [warning_threshold] [critical_threshold]")
+                print("Check types: totalthreads, stuckthreads and serverhealth")
+                print("Do not specify any thresholds for serverhealth")
                 sys.exit(EXIT_UNKNOWN)
-            elif sys.argv[3]=='serverhealth':
-                output,exit = ServerHealth(SetBaseServer(sys.argv[1],sys.argv[2]))
             else:
                 print("Syntax error. Try --help")
                 sys.exit(EXIT_UNKNOWN)
         elif len(sys.argv) is 6:
-            if sys.argv[3]=='totalthread':
-                output,exit = TotalThread(SetBaseServer(sys.argv[1],sys.argv[2]),sys.argv[4],sys.argv[5])
-            elif sys.argv[3]=='stuckthreads':
-                output,exit = StuckThreads(SetBaseServer(sys.argv[1],sys.argv[2]),sys.argv[4],sys.argv[5])
+            if sys.argv[5]=='serverhealth':
+                output,exit = ServerHealth(SetBaseServer(sys.argv[1],sys.argv[2]),sys.argv[3],sys.argv[4])
+            else:
+                print("Syntax error. Try --help")
+                sys.exit(EXIT_UNKNOWN)
+        elif len(sys.argv) is 8:
+            if sys.argv[5]=='totalthreads':
+                output,exit = TotalThreads(SetBaseServer(sys.argv[1],sys.argv[2]),sys.argv[6],sys.argv[7],sys.argv[3],sys.argv[4])
+            elif sys.argv[5]=='stuckthreads':
+                output,exit = StuckThreads(SetBaseServer(sys.argv[1],sys.argv[2]),sys.argv[6],sys.argv[7],sys.argv[3],sys.argv[4])
             else:
                 print("Syntax error. Try --help")
                 sys.exit(EXIT_UNKNOWN)
         else:
-            print("Wrong number of arguments passed to script. Try --help")
+            print("Syntax error. Try --help")
             sys.exit(EXIT_UNKNOWN)
 
         print output
@@ -45,7 +44,7 @@ def main():
 def SetBaseServer(srv_address, port):
     return "http://" + str(srv_address) + ":" + str(port)
 
-def AuthenticateWithWLS(url):
+def AuthenticateWithWLS(url, username, password):
     #HTTP authentication
     try:
         request = urllib2.Request(url)
@@ -66,7 +65,7 @@ def FetchAndParse(request):
             print ("Error in FetchAndParse(): ", str(e))
             sys.exit(EXIT_UNKNOWN)
 
-def StuckThreads(baseserver,warn, crit):
+def StuckThreads(baseserver,warn,crit,username,password):
     try:
         warning_threshold = int(warn)
         critical_threshold = int(crit)
@@ -81,7 +80,7 @@ def StuckThreads(baseserver,warn, crit):
 
         targeturl1 = baseserver + "/management/wls/latest/servers"
 
-        request1 = AuthenticateWithWLS(targeturl1)
+        request1 = AuthenticateWithWLS(targeturl1,username,password)
         data1 = FetchAndParse(request1)
         exit = EXIT_OK
         result = ""
@@ -92,7 +91,7 @@ def StuckThreads(baseserver,warn, crit):
                 sys.exit(EXIT_CRITICAL)
 
             targeturl2 = baseserver + "/management/weblogic/latest/domainRuntime/serverRuntimes/"+server['name']+"/threadPoolRuntime"
-            request2 = AuthenticateWithWLS(targeturl2)
+            request2 = AuthenticateWithWLS(targeturl2,username,password)
             data2 = FetchAndParse(request2)
 
             if int(data2['stuckThreadCount']) >= warning_threshold and int(data2['stuckThreadCount']) < critical_threshold:
@@ -112,10 +111,10 @@ def StuckThreads(baseserver,warn, crit):
         print ("Error in StuckThreads(): ",str(e))
         sys.exit(EXIT_UNKNOWN)
 
-def ServerHealth(baseserver):
+def ServerHealth(baseserver,username,password):
     try:
         targeturl = baseserver + "/management/wls/latest/servers"
-        request = AuthenticateWithWLS(targeturl)
+        request = AuthenticateWithWLS(targeturl,username,password)
         data = FetchAndParse(request)
         exit = EXIT_OK
         result = ""
@@ -137,13 +136,13 @@ def ServerHealth(baseserver):
         print ("Error in ServerHealth()",str(e))
         sys.exit(EXIT_UNKNOWN)
 
-def TotalThread(baseserver, warn, crit):
+def TotalThreads(baseserver, warn, crit,username,password):
 
     try:
         critical_threshold = int(crit)
         warning_threshold = int(warn)
     except Exception as e:
-        print ("Error in TotalThread() while setting crit and warn variables: ", str(e))
+        print ("Error in totalthreads() while setting crit and warn variables: ", str(e))
         sys.exit(EXIT_UNKNOWN)
 
     try:
@@ -152,18 +151,18 @@ def TotalThread(baseserver, warn, crit):
             sys.exit(EXIT_UNKNOWN)
 
         targeturl = baseserver + "/management/wls/latest"
-        request = AuthenticateWithWLS(targeturl)
+        request = AuthenticateWithWLS(targeturl,username,password)
         data = FetchAndParse(request)
 
-        totalThreadCount = data['item']['activeThreadCount']
+        totalthreadsCount = data['item']['activeThreadCount']
         serverName = data['item']['name']
-        result = "Total thread count: " + str(totalThreadCount) + " on " + serverName
+        result = "Total thread count: " + str(totalthreadsCount) + " on " + serverName
 
         exit = EXIT_OK
-        if totalThreadCount >= critical_threshold:
+        if totalthreadsCount >= critical_threshold:
                 result = "CRITICAL: "  + result
                 exit = EXIT_CRITICAL
-        elif totalThreadCount >= warning_threshold and totalThreadCount < critical_threshold:
+        elif totalthreadsCount >= warning_threshold and totalthreadsCount < critical_threshold:
                 result = "WARNING: "  + result
                 exit = EXIT_WARNING
         else:
@@ -172,7 +171,7 @@ def TotalThread(baseserver, warn, crit):
         return (result, exit)
 
     except Exception as e:
-        print ("Error in TotalThread()", str(e))
+        print ("Error in totalthreads()", str(e))
         sys.exit(EXIT_UNKNOWN)
 
 
